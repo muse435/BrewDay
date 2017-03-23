2476821d6092de492f7769397b104819bd48fda9
muse435 <muse435@gmail.com>
2017-03-22 21:49:02 -0400

    Create BrewDay.py

---
+++ BrewDay.py
@@ -0,0 +1,12 @@
+import pygame
+ 
+# pygame
+white = pygame.Color(255,255,255)
+black = pygame.Color(0,0,0)
+pygame.init()
+pygame.display.init()
+screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+pygame.mouse.set_visible(0)
+ 
+
+pygame.quit()
